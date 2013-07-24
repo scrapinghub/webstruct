6377334fbde8a0ab5ac254fc16d3abeb3da86804
Mikhail Korobov <kmike84@gmail.com>
2013-07-25 05:11:22 +0600

    HTML cleaning script handles encodings better

--- webstruct_data/clean_html.py
+++ webstruct_data/clean_html.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-HTML cleaning script. It strips <script>, <style> and alike tags
-and converts page to UTF-8 (regardless of encoding specified in meta tag,
-without changing that meta tag).
+HTML cleaning script. It:
 
-Existing files in output folder are overwritten.
+* strips <script>, <style> and alike tags;
+* converts page to UTF-8 (regardless of encoding specified in meta tag,
+  without changing that meta tag).
+
+Existing files in output folder are silently overwritten.
 
 Usage:
   clean_html.py [--out=<folder>] <input> ...
@@ -20,11 +22,13 @@ Options:
 import codecs
 import os
 import lxml.html.clean
+import lxml.etree
+from lxml.html import HTMLParser
 from w3lib.encoding import html_to_unicode
 from docopt import docopt
 
 
-def clean_html(html):
+def clean_html(html, encoding=None):
     cleaner = lxml.html.clean.Cleaner(
         style=True,
         scripts=True,
@@ -35,7 +39,10 @@ def clean_html(html):
         meta=False,
         safe_attrs_only=False
     )
-    return cleaner.clean_html(html)
+    parser = HTMLParser(encoding=encoding)
+    html = lxml.html.document_fromstring(html, parser=parser)
+    doc = cleaner.clean_html(html)
+    return lxml.etree.tounicode(doc)
 
 
 def mkdir(path):
@@ -56,7 +63,7 @@ if __name__ == '__main__':
         with open(in_name, 'rb') as f:
             encoding, html = html_to_unicode(None, f.read())
 
-        cleaned = clean_html(html)
+        cleaned = clean_html(html.encode(encoding), encoding)
 
         with codecs.open(out_name, 'w', encoding='utf8') as out:
             out.write(cleaned)
