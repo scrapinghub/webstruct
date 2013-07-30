9fdb0628f043508d25e8945a4b6054894874a4b2
tpeng <pengtaoo@gmail.com>
2013-07-30 10:04:23 +0800

    merge address IOE tags

--- block_model/test.py
+++ block_model/test.py
@@ -6,6 +6,7 @@ from webstruct.tagger import Tagger
 tagger = Tagger('webstruct.model')
 
 html = pq(url=sys.argv[1]).html()
-texts, labels = tagger.tag(html)
+texts, labels, address = tagger.tag(html)
 for i, text in enumerate(texts):
     print >> sys.stderr, '[%s] [%s]: %s' %(i, labels[i], text)
+print 'ADDR:', address
