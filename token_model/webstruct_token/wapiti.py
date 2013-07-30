2e773cc023381e0af576cc634602c1efb5aeab17
Mikhail Korobov <kmike84@gmail.com>
2013-07-30 06:07:05 +0600

    wapiti helper

---
+++ token_model/webstruct_token/wapiti.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides some utils for easier creation of Wapiti_ templates
+and data files.
+
+.. _Wapiti: http://wapiti.limsi.fr/
+"""
+
+from __future__ import absolute_import
+import re
+from sklearn.base import BaseEstimator, TransformerMixin
+from .utils import get_combined_keys, tostr
+
+
+class WapitiFeatureEncoder(BaseEstimator, TransformerMixin):
+
+    def __init__(self, move_to_front=('token',)):
+        self.move_to_front = tuple(move_to_front)
+        self.feature_names_ = None
+        self.vocabulary_ = None
+
+    def fit(self, X, y=None):
+        """
+        X should be a list of dicts with features;
+        It can be obtained using HtmlFeaturesExtractor.
+        """
+        return self.partial_fit(X)
+
+    def partial_fit(self, X):
+        keys = self.feature_names_ or set()
+        keys = (set(keys) | get_combined_keys(X)) - set(self.move_to_front)
+        self.feature_names_ = self.move_to_front + tuple(keys)
+        self.vocabulary_ = dict((f, i) for i, f in enumerate(self.feature_names_))
+        return self
+
+    def transform(self, X):
+        """
+        Transform a sequence of dicts X to a list of Wapiti data file lines.
+        """
+        lines = []
+        for dct in X:
+            line = ' '.join(tostr(dct.get(key)) for key in self.feature_names_)
+            lines.append(line)
+        return lines
+
+    def prepare_template(self, template):
+        r"""
+        Prepare Wapiti template by replacing feature names with feature
+        column indices inside ``%x[row,col]`` macros. Indices are compatible
+        with :meth:`WapitiFeatureEncoder.transform` output.
+
+            >>> we = WapitiFeatureEncoder(['token', 'tag'])
+            >>> we.fit([{'token': 'the', 'tag': 'DT'}, {'token': 'dog', 'tag': 'NN'}])
+            WapitiFeatureEncoder(move_to_front=('token', 'tag'))
+            >>> we.prepare_template('*:Pos-1 L=%x[-1, tag]\n*:Suf-2 X=%m[ 0,token,".?.?$"]')
+            '*:Pos-1 L=%x[-1,1]\n*:Suf-2 X=%m[0,0,".?.?$"]'
+
+        Check these links for more info about template format:
+
+        * http://wapiti.limsi.fr/manual.html
+        * http://crfpp.googlecode.com/svn/trunk/doc/index.html#templ
+
+        """
+        return prepare_wapiti_template(template, self.vocabulary_)
+
+    def unigram_features_template(self, scope='*'):
+        """
+        Return Wapiti template with unigram features for each of
+        known features.
+
+            >>> we = WapitiFeatureEncoder(['token', 'tag'])
+            >>> we.fit([{'token': 'the', 'tag': 'DT'}, {'token': 'dog', 'tag': 'NN'}])
+            WapitiFeatureEncoder(move_to_front=('token', 'tag'))
+            >>> print(we.unigram_features_template())
+            # Unigrams for all custom features
+            *:feat-token=%x[0,0]
+            *:feat-tag=%x[0,1]
+            >>> print(we.unigram_features_template('u'))
+            # Unigrams for all custom features
+            u:feat-token=%x[0,0]
+            u:feat-tag=%x[0,1]
+
+        """
+        lines = ['\n# Unigrams for all custom features']
+        for col, name in enumerate(self.feature_names_):
+            line = '{scope}feat:{name}=%x[0,{col}]'.format(scope=scope, name=name, col=col)
+            lines.append(line)
+        return "\n".join(lines) + '\n'
+
+
+WAPITI_MACRO_PATTERN = re.compile(r'''
+    (?P<macro>%[xXtTmM])
+    \[
+    \s*(?P<offset>[-]?\d+)\s*
+    ,
+    \s*(?P<column>[^\],\s]+)\s*  # identifier: anything but closing bracket or comma
+    (?P<rest>[\],])              # closing bracket or comma
+    ''', re.VERBOSE | re.UNICODE
+)
+
+def prepare_wapiti_template(template, vocabulary):
+    r"""
+    Prepare Wapiti template by replacing feature names with feature
+    column indices inside ``%x[row,col]`` macros::
+
+        >>> vocab = {'token': 0, 'tag': 1}
+        >>> prepare_wapiti_template('*:Pos-1 L=%x[-1, tag]\n*:Suf-2 X=%m[ 0,token,".?.?$"]', vocab)
+        '*:Pos-1 L=%x[-1,1]\n*:Suf-2 X=%m[0,0,".?.?$"]'
+
+    Check these links for more info about template format:
+
+    * http://wapiti.limsi.fr/manual.html
+    * http://crfpp.googlecode.com/svn/trunk/doc/index.html#templ
+
+    """
+    def repl(m):
+        column = m.group('column')
+        if not column.isdigit():
+            column = vocabulary[column]
+        return "{0[macro]}[{0[offset]},{1}{0[rest]}".format(m.groupdict(), column)
+
+    return WAPITI_MACRO_PATTERN.sub(repl, template)
+
+
