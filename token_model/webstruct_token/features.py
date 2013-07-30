50dd56af98fd42fcf5187fbfdb42b0d97b8b33e4
Mikhail Korobov <kmike84@gmail.com>
2013-07-30 06:30:28 +0600

    some feature functions

--- token_model/webstruct_token/features.py
+++ token_model/webstruct_token/features.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
+from __future__ import absolute_import, division
+import re
 from .utils import merge_dicts
 
 class CombinedFeatures(object):
@@ -48,13 +49,18 @@ def token_shape(index, tokens, elem, is_tail):
     return {
         'token': token,
         'lower': token.lower(),
-        'title?': token.istitle(),
+        'shape': _shape(token),
+        'endswith_dot': token.endswith('.') and token != '.',
+        'first_upper': token[0].isupper(),
+        'has_copyright': u'©' in token
     }
 
 
 def parent_tag(index, tokens, elem, is_tail):
     return {'parent_tag': elem.tag if not is_tail else elem.getparent().tag}
 
+def inside_a_tag(index, token, elem, is_tail):
+    return {'inside_a_tag': any(e is not None for e in elem.iterancestors('a'))}
 
 def borders(index, tokens, elem, is_tail):
     return {
@@ -62,9 +68,87 @@ def borders(index, tokens, elem, is_tail):
         'border_at_right': index == len(tokens)-1,
     }
 
+def number_pattern(index, tokens, elem, is_tail):
+    token = tokens[index]
+    digit_ratio = sum(1 for ch in token if ch.isdigit()) / len(token)
+
+    if digit_ratio >= 0.3:
+        num_pattern = re.sub('\d', 'X', token)
+        num_pattern2 = re.sub('[^X\W]', 'C', num_pattern)
+        return {
+            'num_pattern': num_pattern,
+            'num_pattern2': num_pattern2,
+        }
+    else:
+        return {}
+
+
+def prefixes_and_suffixes(index, tokens, elem, is_tail):
+    token = tokens[index].lower()
+    return {
+        'prefix2': token[:2],
+        'suffix2': token[-2:],
+        'prefix3': token[:3],
+        'suffix3': token[-3:],
+        'prefix4': token[:4],
+        'suffix4': token[-4:],
+    }
+
+
+def block_length(index, tokens, elem, is_tail):
+    if len(tokens) == 1:
+        bl = '1'
+    elif 1 < len(tokens) <= 10:
+        bl = 'short'
+    elif 10 < len(tokens) <= 20:
+        bl = 'medium'
+    else:
+        bl = 'large'
+    return {'block_length': bl}
+
+
+# stolen from NLTK source (nltk.tag.sequential.ClassifierBasedPOSTagger)
+def _shape(token):
+    if re.match('[0-9]+(\.[0-9]*)?|[0-9]*\.[0-9]+$', token):
+        return 'number'
+    elif re.match('\W+$', token):
+        return 'punct'
+    elif re.match('[A-Z][a-z]+$', token):
+        return 'upcase'
+    elif re.match('[a-z]+$', token):
+        return 'downcase'
+    elif re.match('\w+$', token):
+        return 'mixedcase'
+    else:
+        return 'other'
+
+
+EMAIL_RE = re.compile(
+        r"([-!#$%&'*+/=?^_`{}|~0-9A-Z]+(\.[-!#$%&'*+/=?^_`{}|~0-9A-Z]+)*"  # dot-atom
+        r'|"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
+        r')@(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?', re.IGNORECASE)  # domain
+MONTHS_RE = re.compile('''
+    January|February|March|April|May|June|July|August|September|October|November|December
+    |Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Sept|Oct|Nov|Dec
+    ''',
+    re.VERBOSE | re.IGNORECASE)
+
+
+def token_looks_like(index, tokens, elem, is_tail):
+    token = tokens[0]
+    return {
+        'looks_like_email': EMAIL_RE.search(token) is not None,
+        'looks_like_year': token.isdigit() and len(token)==4 and token[:2] in ['19', '20'],
+        'looks_like_month': MONTHS_RE.match(token) is not None,
+    }
 
 DEFAULT = CombinedFeatures(
     token_shape,
     parent_tag,
-    borders
+    inside_a_tag,
+    borders,
+    number_pattern,
+    prefixes_and_suffixes,
+    block_length,
+    token_looks_like,
 )
