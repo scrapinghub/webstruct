8392112474f84c5acf1a17eec8819bdcc0c82fbe
Mikhail Korobov <kmike84@gmail.com>
2013-07-31 07:27:20 +0600

    change tokenization rules again: don't split : and don't handle contractions

--- token_model/webstruct_token/tokenize.py
+++ token_model/webstruct_token/tokenize.py
@@ -7,14 +7,14 @@ from nltk.tokenize.treebank import TreebankWordTokenizer
 class WordTokenizer(TreebankWordTokenizer):
     """
     This tokenizer is copy-pasted version of TreebankWordTokenizer
-    that doesn't split on @ symbol::
+    that doesn't split on @ and ':' symbols and doesn't split contractions::
 
         >>> from nltk.tokenize.treebank import TreebankWordTokenizer
         >>> s = '''Good muffins cost $3.88\\nin New York. Email: muffins@gmail.com'''
         >>> TreebankWordTokenizer().tokenize(s)
         ['Good', 'muffins', 'cost', '$', '3.88', 'in', 'New', 'York.', 'Email', ':', 'muffins', '@', 'gmail.com']
         >>> WordTokenizer().tokenize(s)
-        ['Good', 'muffins', 'cost', '$', '3.88', 'in', 'New', 'York.', 'Email', ':', 'muffins@gmail.com']
+        ['Good', 'muffins', 'cost', '$', '3.88', 'in', 'New', 'York.', 'Email:', 'muffins@gmail.com']
 
     """
     def tokenize(self, text):
@@ -24,12 +24,10 @@ class WordTokenizer(TreebankWordTokenizer):
         text = re.sub(r'([ (\[{<])"', r'\1 `` ', text)
 
         #punctuation
-        text = re.sub(r'([:,])([^\d])', r' \1 \2', text)
+        text = re.sub(r'([,])([^\d])', r' \1 \2', text)     # CHANGED :
         text = re.sub(r'\.\.\.', r' ... ', text)
+        text = re.sub(r'[;#$%&]', r' \g<0> ', text)         # CHANGED @
 
-        # vvvvvvvvvvvvvvvv this line was changed vvvvvvvvvvvvvvvvvvvvvvvvvvv
-        text = re.sub(r'[;#$%&]', r' \g<0> ', text)
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
         text = re.sub(r'([^\.])(\.)([\]\)}>"\']*)\s*$', r'\1 \2\3 ', text)
         text = re.sub(r'[?!]', r' \g<0> ', text)
@@ -47,14 +45,16 @@ class WordTokenizer(TreebankWordTokenizer):
         text = re.sub(r'"', " '' ", text)
         text = re.sub(r'(\S)(\'\')', r'\1 \2 ', text)
 
-        text = re.sub(r"([^' ])('[sS]|'[mM]|'[dD]|') ", r"\1 \2 ", text)
-        text = re.sub(r"([^' ])('ll|'LL|'re|'RE|'ve|'VE|n't|N'T) ", r"\1 \2 ",
-                      text)
+        # CHANGED:
 
-        for regexp in self.CONTRACTIONS2:
-            text = regexp.sub(r' \1 \2 ', text)
-        for regexp in self.CONTRACTIONS3:
-            text = regexp.sub(r' \1 \2 ', text)
+        # text = re.sub(r"([^' ])('[sS]|'[mM]|'[dD]|') ", r"\1 \2 ", text)
+        # text = re.sub(r"([^' ])('ll|'LL|'re|'RE|'ve|'VE|n't|N'T) ", r"\1 \2 ",
+        #               text)
+        #
+        # for regexp in self.CONTRACTIONS2:
+        #     text = regexp.sub(r' \1 \2 ', text)
+        # for regexp in self.CONTRACTIONS3:
+        #     text = regexp.sub(r' \1 \2 ', text)
 
         # We are not using CONTRACTIONS4 since
         # they are also commented out in the SED scripts
