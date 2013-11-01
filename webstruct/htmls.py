b843994eeed254c2b742bc11474ceedc1c899d43
tpeng <pengtaoo@gmail.com>
2013-11-01 14:57:00 +0800

    add  utils to do more cleanups in html
     e.g. replace the header to more general tag.
     the idea is to have make websturct works on a "generalized" html.

---
+++ webstruct/htmls.py
@@ -0,0 +1,45 @@
+from lxml.etree import iterwalk
+
+
+def replace_tags(root, tags, name):
+    """
+    Replace lxml element's tag.
+
+    >>> from lxml.html import fragment_fromstring, document_fromstring, tostring
+    >>> root = fragment_fromstring('<h1>head 1</h1>')
+    >>> root = replace_tags(root, {'h1'}, 'strong')
+    >>> tostring(root)
+    '<strong>head 1</strong>'
+
+    >>> root = document_fromstring('<h1>head 1</h1> <h2>head 2</h2>')
+    >>> root = replace_tags(root, {'h1','h2','h3','h4'}, 'strong')
+    >>> tostring(root)
+    '<html><body><strong>head 1</strong> <strong>head 2</strong></body></html>'
+    """
+    for tag in tags:
+        for e in root.iter(tag):
+            e.tag = name
+    return root
+
+def kill_tags(root, tags, keep_child=True):
+    """
+    >>> from lxml.html import fragment_fromstring, tostring
+    >>> root = fragment_fromstring('<div><h1>head 1</h1></div>')
+    >>> root = kill_tags(root, {'h1'})
+    >>> tostring(root)
+    '<div>head 1</div>'
+
+    >>> root = fragment_fromstring('<div><h1>head 1</h1></div>')
+    >>> root = kill_tags(root, {'h1'}, False)
+    >>> tostring(root)
+    '<div></div>'
+    """
+    for _, elem in iterwalk(root):
+        if elem.tag in tags:
+            if keep_child:
+                elem.drop_tag()
+            else:
+                elem.drop_tree()
+    return root
+
+
