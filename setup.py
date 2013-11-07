c048a6b622ab70026e607d4b583be4df99e11f55
tpeng <pengtaoo@gmail.com>
2013-11-07 11:33:49 +0800

    update document and bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webstruct',
-    version='0.1.1',
+    version='0.1.2',
     author='Mikhail Korobov, Terry Peng',
     author_email='kmike84@gmail.com, pengtaoo@gmail.com',
     packages=find_packages(),
