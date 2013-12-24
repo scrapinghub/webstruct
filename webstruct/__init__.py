ccaf72390f85e5531ecdf574ee9f11bb3e9739ad
Mikhail Korobov <kmike84@gmail.com>
2013-12-25 00:18:46 +0600

    HtmlLoader

--- webstruct/__init__.py
+++ webstruct/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from .loaders import WebAnnotatorLoader, GateLoader
+from .loaders import WebAnnotatorLoader, GateLoader, HtmlLoader, html_document_fromstring
 from .sequence_encoding import IobEncoder, InputTokenProcessor
 from .feature_extraction import HtmlFeatureExtractor, HtmlTokenizer, HtmlToken
