340dba572e4f8dfc7b6fd64edc09a309814a1e6c
Mikhail Korobov <kmike84@gmail.com>
2013-12-26 10:04:46 +0600

    add WapitiCRF to top-level exports

--- webstruct/__init__.py
+++ webstruct/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from .loaders import WebAnnotatorLoader, GateLoader, HtmlLoader, html_document_fromstring
 from .sequence_encoding import IobEncoder, InputTokenProcessor
 from .feature_extraction import HtmlFeatureExtractor, HtmlTokenizer, HtmlToken
+from .wapiti import WapitiCRF
