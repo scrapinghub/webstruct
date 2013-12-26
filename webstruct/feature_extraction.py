00c328ea2b3085ad1d94eabd0ecec11dffcb9a17
Mikhail Korobov <kmike84@gmail.com>
2013-12-26 10:03:00 +0600

    make HtmlTokenizer and HtmlFeatureExtractor work on lists of trees by default

--- webstruct/feature_extraction.py
+++ webstruct/feature_extraction.py
@@ -2,8 +2,9 @@
 from __future__ import absolute_import
 import copy
 from collections import namedtuple
-from .sequence_encoding import IobEncoder
-from .tokenizers import tokenize
+from sklearn.base import BaseEstimator, TransformerMixin
+from webstruct.sequence_encoding import IobEncoder
+from webstruct.tokenizers import tokenize
 from webstruct.features import CombinedFeatures
 from webstruct.utils import replace_html_tags, kill_html_tags
 
@@ -38,7 +39,7 @@ class HtmlToken(_HtmlToken):
 
 class HtmlTokenizer(object):
     """
-    Use ``HtmlTokenizer.tokenize`` to convert HTML tree (returned by one
+    Use ``HtmlTokenizer.tokenize_single`` to convert HTML tree (returned by one
     of the webstruct loaders) to lists of HtmlToken instances
     and associated tags::
 
@@ -46,7 +47,7 @@ class HtmlTokenizer(object):
         >>> loader = GateLoader(known_tags=['PER'])
         >>> html_tokenizer = HtmlTokenizer(replace_html_tags={'b': 'strong'})
         >>> tree = loader.loadbytes(b"<p>hello, <PER>John <b>Doe</b></PER> <br> <PER>Mary</PER> said</p>")
-        >>> html_tokens, tags = html_tokenizer.tokenize(tree)
+        >>> html_tokens, tags = html_tokenizer.tokenize_single(tree)
         >>> html_tokens  # doctest: +ELLIPSIS
         [HtmlToken(index=0, tokens=[u'hello', u'John'], elem=<Element p at ...>, is_tail=False), HtmlToken...]
         >>> tags
@@ -61,21 +62,21 @@ class HtmlTokenizer(object):
 
     For HTML without text it returns empty lists::
 
-        >>> html_tokenizer.tokenize(loader.loadbytes(b'<p></p>'))
+        >>> html_tokenizer.tokenize_single(loader.loadbytes(b'<p></p>'))
         ([], [])
 
     """
-    def __init__(self, tagset=None, sequence_encoder=None, text_tokenize=None,
+    def __init__(self, tagset=None, sequence_encoder=None, text_tokenize_func=None,
                  kill_html_tags=None, replace_html_tags=None):
         self.tagset_ = set(tagset) if tagset is not None else None
-        self.text_tokenize_ = text_tokenize or tokenize
+        self.text_tokenize_func_ = text_tokenize_func or tokenize
         self.kill_html_tags_ = kill_html_tags
         self.replace_html_tags_ = replace_html_tags
 
         # FIXME: don't use shared instance of sequence encoder
         self.sequence_encoder_ = sequence_encoder or IobEncoder()
 
-    def tokenize(self, tree):
+    def tokenize_single(self, tree):
         """
         Return two lists:
 
@@ -91,6 +92,14 @@ class HtmlTokenizer(object):
         if not res:
             return ([], [])
         return list(res[0]), list(res[1])
+
+    def tokenize(self, trees):
+        X, y = [], []
+        for tree in trees:
+            html_tokens, tags = self.tokenize_single(tree)
+            X.append(html_tokens)
+            y.append(tags)
+        return X, y
 
     def _prepare_tree(self, tree):
         if self.kill_html_tags_:
@@ -113,7 +122,7 @@ class HtmlTokenizer(object):
             yield HtmlToken(index, tail_tokens, tree, True), tag
 
     def _tokenize_and_split(self, text):
-        input_tokens = self._limit_tags(self.text_tokenize_(text or ''))
+        input_tokens = self._limit_tags(self.text_tokenize_func_(text or ''))
         input_tokens = map(unicode, input_tokens)
         return self.sequence_encoder_.encode_split(input_tokens)
 
@@ -129,7 +138,7 @@ class HtmlTokenizer(object):
         ]
 
 
-class HtmlFeatureExtractor(object):
+class HtmlFeatureExtractor(BaseEstimator, TransformerMixin):
     """
     This class extracts features from lists of HtmlTokens
     (from html trees tokenized using HtmlTokenizer).
@@ -163,8 +172,8 @@ class HtmlFeatureExtractor(object):
         >>> feature_extractor = HtmlFeatureExtractor(token_features=[parent_tag])
 
         >>> tree = loader.loadbytes(b"<p>hello, <PER>John <b>Doe</b></PER> <br> <PER>Mary</PER> said</p>")
-        >>> html_tokens, tags = html_tokenizer.tokenize(tree)
-        >>> feature_dicts = feature_extractor.transform(html_tokens)
+        >>> html_tokens, tags = html_tokenizer.tokenize_single(tree)
+        >>> feature_dicts = feature_extractor.transform_single(html_tokens)
         >>> for token, tag, feat in zip(html_tokens, tags, feature_dicts):
         ...     print("%s %s %s" % (token.token, tag, feat))
         hello O {'parent_tag': 'p'}
@@ -178,7 +187,13 @@ class HtmlFeatureExtractor(object):
         self.feature_func_ = CombinedFeatures(*token_features)
         self.global_features_ = global_features or []
 
-    def transform(self, html_tokens):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return [self.transform_single(html_tokens) for html_tokens in X]
+
+    def transform_single(self, html_tokens):
         token_data = list(zip(html_tokens, map(self.feature_func_, html_tokens)))
 
         for feat in self.global_features_:
