e4e2ee0249dacc8d5e7f2c0d134014bcb2ec09ed
Mikhail Korobov <kmike84@gmail.com>
2013-12-27 05:16:22 +0600

    remove WapitiChunker; add transform and score methods to WapitiCRF

--- webstruct/wapiti.py
+++ webstruct/wapiti.py
@@ -16,56 +16,182 @@ import re
 import shlex
 import tempfile
 from sklearn.base import BaseEstimator, TransformerMixin
-from .utils import get_combined_keys, tostr, run_command
+from webstruct.utils import get_combined_keys, tostr, run_command
+from webstruct.metrics import avg_bio_f1_score
 
 
-class WapitiChunker(BaseEstimator):
+class WapitiCRF(BaseEstimator, TransformerMixin):
     """
-    Class for tagging using pre-built Wapiti models.
+    Class for training and applying Wapiti CRF models.
+
+    For training it relies on calling original Wapiti binary (via
+    subprocess), so "wapiti" binary must be available if you need "fit"
+    method.
+
+    For prediction it relies on python-wapiti library.
     """
-    def __init__(self, model, feature_encoder, feature_extractor):
+
+    WAPITI_CMD = 'wapiti'
+
+    def __init__(self, model_filename, wapiti_args=(),
+                 feature_template="# Label ungigrams and bigrams:\n*",
+                 unigrams_scope="u", tempdir=None, unlink_temp=True,
+                 verbose=True, feature_encoder=None):
+
+        self.model_filename = model_filename
+        if isinstance(wapiti_args, (list, tuple)):
+            self.wapiti_args = wapiti_args
+        else:
+            self.wapiti_args = shlex.split(wapiti_args)
+        self.feature_template = feature_template
+        self.unigrams_scope = unigrams_scope
+        self.tempdir = tempdir
+        self.unlink_temp = unlink_temp
+        self.verbose = verbose
+        self._wapiti_model = None
+
+        self.feature_encoder_ = feature_encoder or WapitiFeatureEncoder()
+        super(WapitiCRF, self).__init__()
+
+    def fit(self, X, y, X_dev=None, y_dev=None, out_dev=None):
         """
+        Train a model.
 
         Parameters
         ----------
-        model: wapiti.Model
-            Loaded pre-built Wapiti model.
+        X: a sequence of lists of feature dicts
 
-        feature_encoder: WapitiFeatureEncoder
-            Encoder instance that was used for model building.
+        y: a sequence of lists of labels
 
-        feature_extractor: HtmlFeaturesExtractor
-            Extractor instance that was used for converting HTML into features
+        X_dev: (optional) a sequence of lists of feature dicts
+            Data used for testing and as a stopping criteria.
+
+        y_dev: (optional) a sequence of lists of labels
+            Labels corresponding to X_dev.
+
+        out_dev: (optional) string
+            Path to a file where tagged development data will be written.
 
         """
-        self.model = model
-        self.feature_encoder = feature_encoder
-        self.feature_extractor = feature_extractor
+        if any([X_dev, y_dev, out_dev]):
+            if X_dev is None or y_dev is None:
+                raise ValueError("Pass both X_dev and y_dev to use the development data")
 
-    def transform(self, X, encoding=None):
+        self._wapiti_model = None
+        self.feature_encoder_.reset()
+        self.feature_encoder_.fit(X, y)
+
+        dev_fn = None
+        to_unlink = []
+        try:
+            train_fn = self._create_wapiti_data_file(X, y)
+            to_unlink.append(train_fn)
+
+            if X_dev is not None:
+                dev_fn = self._create_wapiti_data_file(X_dev, y_dev)
+                if out_dev is None:
+                    _, out_dev = tempfile.mkstemp(dir=self.tempdir, suffix=".txt", prefix="wapiti-dev-data")
+                    to_unlink.append(out_dev)
+
+            template_fn = self._create_wapiti_feature_template_file()
+            to_unlink.append(template_fn)
+
+            # run wapiti training
+            args = ['train', '--pattern', template_fn] + self.wapiti_args
+            if dev_fn:
+                args += ['--devel', dev_fn]
+            args += [train_fn, self.model_filename]
+            self.run_wapiti(args)
+
+            # do a final check on development data
+            if dev_fn:
+                args = ['label', '-m', self.model_filename, '--check', dev_fn, out_dev]
+                self.run_wapiti(args)
+
+        finally:
+            if self.unlink_temp:
+                for filename in to_unlink:
+                    os.unlink(filename)
+
+    def transform(self, X):
         """
-        Return a list of (text, ner_label) pairs for HTML document X.
+        Make a prediction.
+
+        Parameters
+        ----------
+        X: a sequence of lists of feature dicts
+
+        Returns
+        -------
+        y: a sequence of lists of labels
+
         """
-        feature_dicts, feature_lines = self._prepare_features(X, encoding)
-        labels = self._get_labels(feature_lines)
-        tokens = [f['token'] for f in feature_dicts]
-        assert len(tokens) == len(labels), (len(tokens), len(labels), tokens, labels)
+        model = self._get_python_wapiti_model()
+        sequences = self._to_wapiti_sequences(X)
+        return [model.label_sequence(seq).splitlines() for seq in sequences]
 
-        le = self.feature_extractor.label_encoder
-        return [
-            (" ".join(ner_tokens), label)
-            for ner_tokens, label in le.group(zip(tokens, labels))
-        ]
+    def score(self, X, y):
+        """
+        Macro-averaged F1 score of lists of BIO-encoded sequences
+        ``y_true`` and ``y_pred``.
 
-    def _prepare_features(self, X, encoding=None):
-        feature_dicts, _ = self.feature_extractor.fit_transform(X, encoding=encoding)
-        feature_lines = self.feature_encoder.transform(feature_dicts)
-        return feature_dicts, feature_lines
+        A named entity in a sequence from ``y_pred`` is considered
+        correct only if it is an exact match of the corresponding entity
+        in the ``y_true``.
 
-    def _get_labels(self, feature_lines):
-        lines_joined = "\n".join(feature_lines)
-        out_lines = self.model.label_sequence(lines_joined).split('\n')
-        return [line.rsplit()[-1] for line in out_lines if line.strip()]
+        It requires https://github.com/larsmans/seqlearn to work.
+        """
+        y_pred = self.transform(X)
+        return avg_bio_f1_score(y, y_pred)
+
+    def run_wapiti(self, args):
+        return run_command([self.WAPITI_CMD] + args, self.verbose)
+
+    def _get_python_wapiti_model(self):
+        if self._wapiti_model is None:
+            self._load_model()
+        return self._wapiti_model
+
+    def _load_model(self):
+        import wapiti
+        if self.model_filename is None:
+            raise ValueError("model filename is unknown, can't load model")
+        self._wapiti_model = wapiti.Model(model=self.model_filename)
+
+    def _to_wapiti_sequences(self, X, y=None):
+        X = self.feature_encoder_.transform(X)
+        if y is None:
+            return ["\n".join(lines) for lines in X]
+        else:
+            return [
+                self._to_train_sequence(lines, tags) for lines, tags in zip(X, y)
+            ]
+
+    def _create_wapiti_data_file(self, X, y=None):
+        """
+        Create a file with input data for wapiti. Return a resulting file name;
+        caller should unlink the file.
+        """
+        with tempfile.NamedTemporaryFile('wb', prefix="wapiti-data-", suffix=".txt", dir=self.tempdir, delete=False) as fp:
+            for seq in self._to_wapiti_sequences(X, y):
+                fp.write(seq.encode('utf8'))
+                fp.write(b"\n\n")
+        return fp.name
+
+    def _create_wapiti_feature_template_file(self):
+        # create feature template
+        with tempfile.NamedTemporaryFile('wb', prefix="feature-template-", suffix=".txt", dir=self.tempdir, delete=False) as fp:
+            template = self.feature_encoder_.prepare_template(self.feature_template)
+            fp.write(template.encode('utf8'))
+
+            if self.unigrams_scope is not None:
+                unigram_template = self.feature_encoder_.unigram_features_template(self.unigrams_scope)
+                fp.write(b"\n")
+                fp.write(unigram_template.encode('utf8'))
+        return fp.name
+
+    def _to_train_sequence(self, wapiti_lines, tags):
+        return "\n".join(["%s %s" %(line, tag) for line, tag in zip(wapiti_lines, tags)])
 
 
 class WapitiFeatureEncoder(BaseEstimator, TransformerMixin):
@@ -158,131 +284,6 @@ class WapitiFeatureEncoder(BaseEstimator, TransformerMixin):
 
     def reset(self):
         self.feature_names_ = None
-
-
-
-class WapitiCRF(BaseEstimator, TransformerMixin):
-    """
-    Class for training and applying Wapiti CRF models.
-
-    For training it relies on calling original Wapiti binary (via
-    subprocess), so "wapiti" binary must be available if you need "fit"
-    method.
-
-    For prediction it relies on python-wapiti library.
-    """
-
-    WAPITI_CMD = 'wapiti'
-
-    def __init__(self, model_filename, wapiti_args,
-                 feature_template, unigrams_scope="u",
-                 tempdir=None, unlink_temp=True, verbose=True):
-
-        self.model_filname_ = model_filename
-        if isinstance(wapiti_args, list):
-            self.wapiti_args_ = wapiti_args
-        else:
-            self.wapiti_args_ = shlex.split(wapiti_args)
-        self.feature_template_ = feature_template
-        self.unigrams_scope_ = unigrams_scope
-        self.tempdir_ = tempdir
-        self.unlink_temp_ = unlink_temp
-        self.verbose_ = verbose
-
-        self.feature_encoder_ = WapitiFeatureEncoder()
-        super(WapitiCRF, self).__init__()
-
-    def fit(self, X, y, X_dev=None, y_dev=None, out_dev=None):
-        """
-        Train a model.
-
-        Parameters
-        ----------
-        X: a sequence of lists of feature dicts
-
-        y: a sequence of lists of labels
-
-        X_dev: (optional) a sequence of lists of feature dicts
-            Data used for testing and as a stopping criteria.
-
-        y_dev: (optional) a sequence of lists of labels
-            Labels corresponding to X_dev.
-
-        out_dev: (optional) string
-            Path to a file where tagged development data will be written.
-
-        """
-        if any([X_dev, y_dev, out_dev]):
-            if X_dev is None or y_dev is None:
-                raise ValueError("Pass both X_dev and y_dev to use the development data")
-
-        self.feature_encoder_.reset()
-        self.feature_encoder_.fit(X, y)
-
-        dev_fn = None
-        to_unlink = []
-        try:
-            train_fn = self._create_wapiti_data_file(X, y)
-            to_unlink.append(train_fn)
-
-            if X_dev is not None:
-                dev_fn = self._create_wapiti_data_file(X_dev, y_dev)
-                if out_dev is None:
-                    _, out_dev = tempfile.mkstemp(dir=self.tempdir_, suffix=".txt", prefix="wapiti-dev-data")
-                    to_unlink.append(out_dev)
-
-            template_fn = self._create_wapiti_feature_template_file()
-            to_unlink.append(template_fn)
-
-            # run wapiti training
-            args = ['train', '--pattern', template_fn] + self.wapiti_args_
-            if dev_fn:
-                args += ['--devel', dev_fn]
-            args += [train_fn, self.model_filname_]
-            self.run_wapiti(args)
-
-            # do a final check on development data
-            if dev_fn:
-                args = ['label', '-m', self.model_filname_, '--check', dev_fn, out_dev]
-                self.run_wapiti(args)
-
-        finally:
-            if self.unlink_temp_:
-                for filename in to_unlink:
-                    os.unlink(filename)
-
-    def run_wapiti(self, args):
-        return run_command([self.WAPITI_CMD] + args, self.verbose_)
-
-    def _create_wapiti_data_file(self, X, y):
-        """
-        Create a file with input data for wapiti. Return a resulting file name;
-        caller should unlink the file.
-        """
-        X = self.feature_encoder_.transform(X)
-        wapiti_sequences = [
-            self._to_train_sequence(lines, tags) for lines, tags in zip(X, y)
-        ]
-        with tempfile.NamedTemporaryFile('wb', prefix="wapiti-data-", suffix=".txt", dir=self.tempdir_, delete=False) as fp:
-            for seq in wapiti_sequences:
-                fp.write(seq.encode('utf8'))
-                fp.write(b"\n\n")
-        return fp.name
-
-    def _create_wapiti_feature_template_file(self):
-        # create feature template
-        with tempfile.NamedTemporaryFile('wb', prefix="feature-template-", suffix=".txt", dir=self.tempdir_, delete=False) as fp:
-            template = self.feature_encoder_.prepare_template(self.feature_template_)
-            fp.write(template.encode('utf8'))
-
-            if self.unigrams_scope_ is not None:
-                unigram_template = self.feature_encoder_.unigram_features_template(self.unigrams_scope_)
-                fp.write(b"\n")
-                fp.write(unigram_template.encode('utf8'))
-        return fp.name
-
-    def _to_train_sequence(self, wapiti_lines, tags):
-        return "\n".join(["%s %s" %(line, tag) for line, tag in zip(wapiti_lines, tags)])
 
 
 
