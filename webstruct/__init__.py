2426fa899801e9d33d44b6cbc4d09ed158d44b9c
Mikhail Korobov <kmike84@gmail.com>
2014-02-25 12:48:11 +0600

    add functions and classes from webstruct.model to top-level namespace

--- webstruct/__init__.py
+++ webstruct/__init__.py
@@ -4,3 +4,4 @@ from .loaders import WebAnnotatorLoader, GateLoader, HtmlLoader, load_trees
 from .sequence_encoding import IobEncoder, InputTokenProcessor
 from .feature_extraction import HtmlFeatureExtractor, HtmlTokenizer, HtmlToken
 from .wapiti import WapitiCRF
+from .model import create_wapiti_pipeline, NER
