8177cb9c207323b221933d3f26b9345c6ff259bf
Mikhail Korobov <kmike84@gmail.com>
2014-02-25 13:28:32 +0600

    allow to build docs without installing lxml and scikit-learn

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,35 @@ sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')
 ))
 
+
+# mock dependencies
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['lxml', 'lxml.html', 'lxml.html.clean', 'lxml.etree',
+                'sklearn', 'sklearn.base', 'sklearn.metrics',
+                'sklearn.pipeline']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
