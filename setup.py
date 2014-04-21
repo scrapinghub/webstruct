ffaf3089b99a86710170af28927e1e4fee36c6d2
Mikhail Korobov <kmike84@gmail.com>
2014-04-21 20:31:31 +0600

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+version = '0.1.2'
+
 setup(
     name='webstruct',
-    version='0.1.2',
+    version=version,
+    description="A library for creating statistical NER systems that work on HTML data",
+    long_description=open('README.rst').read(),
     author='Mikhail Korobov, Terry Peng',
     author_email='kmike84@gmail.com, pengtaoo@gmail.com',
+    url='https://github.com/scrapinghub/webstruct',
     packages=find_packages(),
-    license = 'MIT license',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
 )
