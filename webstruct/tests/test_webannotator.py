ba8e911246b540f0415da765ff96c344e661cc03
Vostretsov Nikita <nikita@scrapinghub.com>
2017-09-26 17:13:59 +0000

    new html tree based to webannotator transformer

--- webstruct/tests/test_webannotator.py
+++ webstruct/tests/test_webannotator.py
@@ -81,6 +81,102 @@ class WaTitleTest(HtmlTest):
 
 
 class WaConvertTest(HtmlTest):
+    def test_wa_convert_crosstitle(self):
+        tree = html_document_fromstring(b"""
+        <html>
+            <head>
+                <title>
+                     __START_ORG__ a __END_ORG__  b  __START_ORG__ a
+                </title>
+            </head>
+            <body>
+                a __END_ORG__ a
+            </body>
+        </html>
+        """)
+        wa_tree = webannotator.to_webannotator(tree)
+        wa_tree_str = tostring(wa_tree)
+
+        self.assertHtmlEqual(wa_tree_str, r"""
+        <html>
+            <head>
+                <title>
+                    a b a
+                </title>
+            </head>
+            <body>
+                <span class="WebAnnotator_ORG" style="color:#000000; background-color:#33CCFF;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                    a
+                </span>
+                a
+            </body>
+            <wa-color id="WA-color-0" bg="#33CCFF" fg="#000000" class="WebAnnotator_ORG" type="ORG"></wa-color>
+            <wa-title style="box-shadow:0 0 1em black;border:2px solid blue;padding:0.5em;">
+                <span class="WebAnnotator_ORG" style="color:#000000; background-color:#33CCFF;" wa-id="0" wa-subtypes="" wa-type="ORG">
+                    a
+                </span>
+                b
+                <span class="WebAnnotator_ORG" style="color:#000000; background-color:#33CCFF;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                    a
+                </span>
+          </wa-title>
+        </html>
+        """)
+    def test_wa_convert_inner(self):
+        tree = html_document_fromstring(b"""
+        <html>
+          <head>
+            <title> __START_PER__ Hello! __END_PER__  world!</title>
+          </head>
+          <body>
+            <p>
+              __START_ORG__ Scrapinghub
+                <b>Inc has</b>an
+                <b>office</b>in Montevideo __END_ORG__  cool
+            </p>
+          </body>
+        </html>
+        """)
+        wa_tree = webannotator.to_webannotator(tree)
+        wa_tree_str = tostring(wa_tree)
+
+        self.assertHtmlEqual(wa_tree_str, r"""
+        <html>
+          <head>
+            <title>Hello! world!</title>
+          </head>
+          <body>
+            <p>
+              <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                  Scrapinghub
+              </span>
+              <b>
+                <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                    Inc has
+                </span>
+              </b>
+              <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                  an
+              </span>
+              <b>
+                  <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                      office
+                  </span>
+              </b>
+              <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="1" wa-subtypes="" wa-type="ORG">
+                  in Montevideo
+              </span>
+               cool
+            </p>
+          </body>
+          <wa-color id="WA-color-0" bg="#33CCFF" fg="#000000" class="WebAnnotator_PER" type="PER"></wa-color>
+          <wa-color id="WA-color-1" bg="#FF0000" fg="#000000" class="WebAnnotator_ORG" type="ORG"></wa-color>
+          <wa-title style="box-shadow:0 0 1em black;border:2px solid blue;padding:0.5em;">
+            <span class="WebAnnotator_PER" style="color:#000000; background-color:#33CCFF;" wa-id="0" wa-subtypes="" wa-type="PER">Hello!</span> world!
+          </wa-title>
+        </html>
+        """)
+
     def test_wa_convert(self):
         tree = html_document_fromstring(b"""
         <html>
@@ -91,7 +187,7 @@ class WaConvertTest(HtmlTest):
             <p>
               __START_ORG__ Scrapinghub
                 <b>Inc __END_ORG__  has</b>an
-                <b>__START_CITY__office</b>in Montevideo __END_CITY__   __START_ORG__ SH __END_ORG__
+                <b> __START_CITY__ office</b>in Montevideo __END_CITY__   __START_ORG__ SH __END_ORG__ cool
             </p>
           </body>
         </html>
@@ -99,7 +195,7 @@ class WaConvertTest(HtmlTest):
         wa_tree = webannotator.to_webannotator(tree)
         wa_tree_str = tostring(wa_tree)
 
-        self.assertHtmlEqual(wa_tree_str, br"""
+        self.assertHtmlEqual(wa_tree_str, r"""
         <html>
           <head>
             <title>Hello! world!</title>
@@ -110,7 +206,7 @@ class WaConvertTest(HtmlTest):
               <b><span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="1" wa-subtypes="" wa-type="ORG">Inc</span> has</b>an
               <b><span class="WebAnnotator_CITY" style="color:#000000; background-color:#33FF33;" wa-id="2" wa-subtypes="" wa-type="CITY">office</span></b>
               <span class="WebAnnotator_CITY" style="color:#000000; background-color:#33FF33;" wa-id="2" wa-subtypes="" wa-type="CITY">in Montevideo</span>
-              <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="3" wa-subtypes="" wa-type="ORG">SH</span>
+              <span class="WebAnnotator_ORG" style="color:#000000; background-color:#FF0000;" wa-id="3" wa-subtypes="" wa-type="ORG">SH</span> cool
             </p>
           </body>
           <wa-color id="WA-color-0" bg="#33CCFF" fg="#000000" class="WebAnnotator_PER" type="PER"></wa-color>
@@ -129,7 +225,7 @@ class WaConvertTest(HtmlTest):
         wa_tree = webannotator.to_webannotator(tree)
         wa_tree_str = tostring(wa_tree)
 
-        self.assertHtmlEqual(wa_tree_str, br"""
+        self.assertHtmlEqual(wa_tree_str, r"""
         <html>
           <body>
             <p>
