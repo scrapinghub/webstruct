257a32f51855901562d2ed21c33777dede86c0d7
Ludovica Gonella <ludovica.gonella@gmail.com>
2018-05-25 16:12:24 +0200

    still debug

--- webstruct/evaluation/evaluation.py
+++ webstruct/evaluation/evaluation.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import six
 
 
 def _get_accuracy(true_entities, pred_entities):
@@ -7,6 +8,11 @@ def _get_accuracy(true_entities, pred_entities):
         if label in pred_entities:
             true_pos = true_entities[label].intersection(pred_entities[label])
             acc[label] = (1.0 * len(true_pos)) / (1.0 * len(true_entities[label]))
+            if six.PY2 and label == 'SUBJ':
+                print "label: ", label
+                print "len(true_pos), true_pos: ", len(true_pos), true_pos
+                print "len(true_entities[label]), true_entities[label]: ", len(true_entities[label]), true_entities[label]
+                print len(true_pos) / len(true_entities[label])
         else:
             acc[label] = 0
     entities_in_common = set(true_entities.keys()).intersection(
@@ -17,6 +23,8 @@ def _get_accuracy(true_entities, pred_entities):
         # should this raise an exception?
         for label in pred_extra_entities:
             acc[label] = 0
+    if six.PY2:
+        print "true_pos: ", acc
     return acc
 
 
