f7d56b0a59486df8142ecce38007b29a6617716f
Ludovica Gonella <ludovica.gonella@gmail.com>
2018-05-25 16:05:57 +0200

    debug

--- webstruct/evaluation/evaluation.py
+++ webstruct/evaluation/evaluation.py
@@ -6,7 +6,7 @@ def _get_accuracy(true_entities, pred_entities):
     for label, entities in true_entities.items():
         if label in pred_entities:
             true_pos = true_entities[label].intersection(pred_entities[label])
-            acc[label] = 1.0 * len(true_pos) / len(true_entities[label])
+            acc[label] = (1.0 * len(true_pos)) / (1.0 * len(true_entities[label]))
         else:
             acc[label] = 0
     entities_in_common = set(true_entities.keys()).intersection(
