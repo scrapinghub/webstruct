844777d09506e8ef6599dedcb664458a91910d44
Ludovica Gonella <ludovica.gonella@gmail.com>
2018-08-30 00:52:31 -0700

    remove gitpython

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
-    install_requires=['six', 'lxml', 'scikit-learn', 'tldextract', 'requests', 'GitPython'],
+    install_requires=['six', 'lxml', 'scikit-learn', 'tldextract', 'requests'],
 )
